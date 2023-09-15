5d66319aae34bba9e6fcafd7864fec0c38e1bf3f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-09-15 08:47:46 +0300

    added a logger using pino js

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import express from 'express';
 import config from 'config'
 import connect from './utils/connect';
+import log from './utils/logger';
 const env = process.env.NODE_ENV;
 
 const port = config.get<string>('port')
 
-console.log(port)
 const app = express();
 
 app.listen(port, async() => {
-    console.log(`app running on port ${port}`)
+    log.info(`app running on port ${port}`)
 
     await connect()
 })
---
+++ src/utils/logger.ts
@@ -0,0 +1,13 @@
+import logger from 'pino'
+import dayjs from 'dayjs'
+
+
+const log= logger({
+
+    base:{
+pid:false
+    },
+    timestamp:() => `."time:"${dayjs().format()}`
+})
+
+export default log
